5f5638b300badb12740663731adbc608fc7900e5
AleonaKS <challengesim@gmail.com>
2026-02-28 00:03:16 +0400

    fundamental fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,21 +1,37 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'kupipodari-backend',
+//       script: 'dist/main.js',          
+//       env: {
+//         NODE_ENV: 'production',
+//         PORT: 4000,
+//         JWT_SECRET: process.env.JWT_SECRET,
+//         POSTGRES_HOST: process.env.POSTGRES_HOST,
+//         POSTGRES_PORT: 5432,
+//         POSTGRES_USER: process.env.POSTGRES_USER,
+//         POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+//         POSTGRES_DB: process.env.POSTGRES_DB,
+//       },
+//       env_production: {
+//         NODE_ENV: 'production',
+//         PORT: 4000,
+//       },
+//     },
+//   ],
+// };
 module.exports = {
   apps: [
     {
-      name: 'kupipodari-backend',
-      script: 'dist/main.js',          
+      name: "kupipodari-backend",
+      script: "./dist/main.js",
+      instances: "max",
+      exec_mode: "cluster",
       env: {
-        NODE_ENV: 'production',
-        PORT: 4000,
-        JWT_SECRET: process.env.JWT_SECRET,
-        POSTGRES_HOST: process.env.POSTGRES_HOST,
-        POSTGRES_PORT: 5432,
-        POSTGRES_USER: process.env.POSTGRES_USER,
-        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-        POSTGRES_DB: process.env.POSTGRES_DB,
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: 'production',
-        PORT: 4000,
+        NODE_ENV: "production",
       },
     },
   ],
