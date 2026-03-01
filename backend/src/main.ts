7fad0e758664c9fe2b1c26b8c8f5cc67f3d220d7
AleonaKS <challengesim@gmail.com>
2026-03-01 19:44:14 +0400

    fundamental fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,28 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ConfigService } from '@nestjs/config';
-// import { ValidationPipe } from '@nestjs/common';
-// import { HttpExceptionFilter } from './filters/http-exception.filter';
-// async function bootstrap() {
-//   try {
-//     const app = await NestFactory.create(AppModule, { cors: true });
-//     const configService = app.get(ConfigService); 
-//     app.useGlobalPipes(
-//       new ValidationPipe({ transform: true, whitelist: true }),
-//     ); 
-//     app.useGlobalFilters(new HttpExceptionFilter());
-//     const port = configService.get('port');
-//     await app.listen(port);
-//     console.log(`Server started on port: ${port}`);
-//   } catch (error) {
-//     console.error('Error when starting the server:', error);
-//     process.exit(1);
-//   }
-// }
-// bootstrap();
-
-
-// src/main.ts
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
